649bbbe6b5c2cbfba96510b1ee4542662f2bdf08
Isa-josep <isaurini1902@gmail.com>
2024-02-09 00:20:37 -0600

    init commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Material App Bar'),
+          actions: [
+            IconButton(
+              onPressed: (){},
+              icon: const Icon(Icons.door_back_door),
+            )
+          ],
+        ),
+        body: Container(
+          padding: const EdgeInsets.all(20),
+          child:  Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                MaterialButton(
+                  splashColor: Colors.transparent,
+                  minWidth: double.infinity,
+                  height: 40,
+                  color: Colors.red,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  onPressed: (){
+                },
+                child: const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(FontAwesomeIcons.google, color: Colors.white, size: 20,),
+                    Text('  Sing In with Google', style: TextStyle(color: Colors.white),),
+                  ], 
+                ),
+                
+                ),
+              ],
+            ),
+          ),
+        )
+      ),
+    );
+  }
+}
